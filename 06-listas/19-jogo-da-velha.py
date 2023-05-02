8004c7451793cc93222b183e101a20f5f8423066
thiagorocco <deroccotr@gmail.com>
2023-05-01 21:52:03 -0300

    7 .o commit - Jogo da velha

--- 06-listas/19-jogo-da-velha.py
+++ 06-listas/19-jogo-da-velha.py
@@ -2,6 +2,12 @@ opcao = 3
 jogador1 = 'X'
 jogador2 = 'O'
 tabuleiro = [[0 for i in range(3)]for j in range(3)]
+linha1 = []
+linha2 = []
+linha3 = []
+col1 = []
+col2 = []
+col3 = []
 
 def imprimir():
     for i in range(3):
@@ -13,12 +19,13 @@ def checagem():
 
     for i in range(3):
         for j in range(3):
-            if i==0:    
-                linha1[j] = matriz[i][j]
+            if i==0:
+                linha1[j] = tabuleiro[i][j]
             if i==1:    
-                linha2[j] = matriz[i][j]
+                linha2[j] = tabuleiro[i][j]
             if i==2:    
-                linha3[j] = matriz[i][j]
+                linha3[j] = tabuleiro[i][j]
+        
 
     if linha1.count('X') == 3:
         print('Jogador 1 é o vencedor')
