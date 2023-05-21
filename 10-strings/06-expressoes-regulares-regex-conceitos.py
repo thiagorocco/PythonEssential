93908baa25b6cdf088e68aec94ca302ce54cdffb
thiagorocco <deroccotr@gmail.com>
2023-05-21 18:55:11 -0300

    32.º commit Regex

--- 10-strings/06-expressoes-regulares-regex-conceitos.py
+++ 10-strings/06-expressoes-regulares-regex-conceitos.py
@@ -1,10 +1,14 @@
 '''
     As regular expressions (também conhecidas pela abreviação Regex) 
     são um padrão de um texto que vamos procurar.
+
+    Elas podem facilitar muito o trabalho do programador
+
+    Vejamos um exemplo sem expressão regular e com expressão regular
 '''
-#Função sem expressão regular
+''' *** Função sem expressão regular *** '''
 def eTelefone(numero):
-    #verifica se tem 9 dígitos
+    #verifica se NÃO tem 9 dígitos
     if len(numero) != 9:
         return False
     #verifica se os dígitos 0, 1, 2 e 3 NÃO são númericos
@@ -14,15 +18,20 @@ def eTelefone(numero):
     #verifica se o 4º dígito NÃO é hífen  
     if numero[4] != '-':
         return False
+    #verifica se os dígitos 5,6,7 e 8 NÃO são númericos
     for index in range(5,9):
         if numero[index].isdecimal() is not True:
             return False
     print("Numero detectado: ", numero)
     return True
+
+controle = True
+
+while controle:
+    numero = input("Numero no formato 'xxxx-yyyy': " )
  
-while True:
- numero = input("Numero no formato 'xxxx-yyyy': " )
- 
- for i in range( len(numero)):
-    pedaco=numero[i:i+9]
-    eTelefone(pedaco)
+    for i in range( len(numero)):
+        pedaco=numero[i:i+9]
+        if(eTelefone(pedaco)):
+            controle = False
+            break
