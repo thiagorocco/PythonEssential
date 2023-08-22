39f5a8cb62e8c39e9fb57a3cc6e418afcfdf0725
thiago-rocco <deroccotr@gmail.com>
2023-08-22 06:49:22 -0300

    Adicionado conteúdo sobre métodos de classe

--- 12-OO/01-como-criar-classes.py
+++ 12-OO/01-como-criar-classes.py
@@ -4,15 +4,24 @@ class MinhaClasse:
     idade = 33
     sexo = 'm'
 
-objeto = MinhaClasse
+    def __init__(self,time_futebol, manequim):
+        self.time_futebol = time_futebol
+        self.manequim = manequim
+
+objeto = MinhaClasse('Coritiba','M')
 print('Nome: ', objeto.nome)
+print(f'Torce para o {objeto.time_futebol} e veste {objeto.manequim}')
 
 
 class Carro:
     def __init__(self):
         print('Carro criado')
 
+    @staticmethod
+    def consumo(a,b):
+        return a*b
 #Objeto instanciado com parenteses e sem parenteses
 #O primeiro vai executar o método construtor enquanto o segundo não
 corolla = Carro()
 civic = Carro
+print('Consumo = ',Carro.consumo(10,2))
