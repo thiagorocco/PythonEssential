a856b0559883ea00b6f3c7167e8b527ed6039368
thiago-rocco <deroccotr@gmail.com>
2023-08-24 06:47:01 -0300

    Métodos getters e setters atualizaso e atributos de classe.

--- 12-OO/05.3-metodos-getters-e-setters.py
+++ 12-OO/05.3-metodos-getters-e-setters.py
@@ -1,4 +1,4 @@
-class Produto:
+'''class Produto:
     def __init__(self, nome, preco):
         self.nome = nome
         self.preco = preco
@@ -24,4 +24,41 @@ print(p1.preco)
 
 p2 = Produto('Caneca','15')
 p2.desconto(10)
-print(p2.preco)
+print(p2.preco)'''
+class Produto:
+    def __init__(self, nome, preco):
+        self._nome = nome  # Atributo privado
+        self._preco = preco  # Atributo privado
+
+    def desconto(self, percentual):
+        self._preco = self._preco - (self._preco * (percentual / 100))
+
+    # Getter
+    @property
+    def nome(self):
+        return self._nome
+
+    # Setter
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor
+
+    # Getter
+    @property
+    def preco(self):
+        return self._preco
+
+    # Setter
+    @preco.setter
+    def preco(self, valor):
+        if isinstance(valor, str):
+            valor = float(valor.replace('R$', ''))
+
+        self._preco = valor
+
+# Exemplo de uso
+produto = Produto("Produto1", 100.0)
+print(f"Nome: {produto.nome}, Preço: R${produto.preco}")
+
+produto.desconto(10)
+print(f"Nome: {produto.nome}, Preço com desconto: R${produto.preco}")
--- 12-OO/08-atributos-de-classe.py
+++ 12-OO/08-atributos-de-classe.py
@@ -1,4 +1,8 @@
 '''
+    Variaveis de classe possuem o mesmo valor para todos os objetos.
+    Se quiser valor diferentes para cada objeto utilize as variáveis de instância(dentro do
+    método __init__)
+
     self.count -> vai atuar em um objeto, especificamente
     Funcionario.count -> todos os objetos vão atuar numa mesma variável, o atributo de classe
 '''
@@ -13,10 +17,25 @@ class Funcionario:
         Funcionario.count += 1
         print('Número de funcionários',Funcionario.count)
 
-op = 1
+class A:
+    vc = 123
+
+    def __init__(self):
+        self.vc = 321
+
+a1 = A()
+a2 = A()
+
+A.vc = 'Alterado'
+print(a1.vc)
+print(a2.vc)
+print(A.vc)
+
+'''op = 1
 func = []
 while op:
     op = int(input('0.Sair\n1.Criar funcionário\n'))
     if op == 1:
         nome = input('Nome: ')
-        func.append(Funcionario(nome))
+        func.append(Funcionario(nome))'''
+
