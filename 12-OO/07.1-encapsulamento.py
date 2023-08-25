63eeea7936307e030c6ee0344560fc0b3fd7288c
thiago-rocco <deroccotr@gmail.com>
2023-08-25 06:52:50 -0300

    encapsulamento

---
+++ 12-OO/07.1-encapsulamento.py
@@ -0,0 +1,28 @@
+'''
+    public - Acessíveis de qualquer lugar, dentro ou fora da classe
+    protected - Acessíveis apenas por classes que herdam a classe pai
+    private - Acessíveis apenas dentro da própria classe
+
+    Esse é o conceito da OO clássica, e como fica isso em Python???
+
+    No Python existem convenções e não restrições:
+
+    _ recomenda mas não impede o acesso diretamente
+    __ impede o acesso direto, porém se usar _NOMEDACLASSE__nomeatributo conseguirá acessar
+
+    O ideal é usar um método getter para acessar esses atributos
+'''
+class BaseDeDados:
+    def __init__(self):
+        self.__dados = {}
+    def inserir_cliente(self, id, nome):
+        if 'clientes' not in self.__dados:
+            self.__dados['clientes'] = {id: nome}
+        else:
+            self.__dados['clientes'].update({id:nome})
+
+bd = BaseDeDados()
+bd.inserir_cliente(1,'Otávio')
+bd.inserir_cliente(2,'Miranda')
+bd.inserir_cliente(3,'Rose')
+print(bd._BaseDeDados__dados)
