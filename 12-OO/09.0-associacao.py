f1fa68e3f167b740780d7335e405bcf335207e89
thiago-rocco <deroccotr@gmail.com>
2023-08-26 09:51:12 -0300

    Conceitos e prática de associações concluídos

--- 12-OO/09.0-associacao.py
+++ 12-OO/09.0-associacao.py
@@ -6,6 +6,17 @@ escritor = Escritor('Joãozinho')
 caneta = Caneta('BIC')
 maquina = MaquinaDeEscrever()
 
-escritor.ferramenta = caneta
+#Esse tipo de associação também é conhecida como "associação fraca" ou "indireta"
+#A associação forte seria referenciar de forma direta; EX: lista = [1,2,3]
+escritor.ferramenta = maquina
 escritor.ferramenta.escrever()
+
+del escritor
+try:
+    escritor.ferramenta.escrever()
+except:
+    print('Objeto escritor foi removido. Não pode usar o método escrever associado.')
+
+caneta.escrever()
+maquina.escrever()
 
