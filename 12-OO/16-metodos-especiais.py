7a988ad1d690c4547add6fffce8c55d6bfc92418
thiago-rocco <deroccotr@gmail.com>
2023-09-05 06:20:38 -0300

    Métodos __str__, __repr__ e __hash__ adicionados ao conteúdo de métodos especiais em Python

---
+++ 12-OO/16-metodos-especiais.py
@@ -0,0 +1,57 @@
+'''
+    MÉTODOS ESPECIAIS (SPECIAL METHODS):
+
+    Estes 2 métodos são muito parecidos, mas tem finalidades diferentes. Ambos
+    retornam uma string ao tentar imprimir o objeto.
+    __str__ é usado para criar uma representação legível por humanos,
+    __repr__ é usado para criar uma representação não ambígua do objeto.
+
+    Você pode implementar um ou ambos, dependendo das suas necessidades,
+    mas é uma boa prática implementar pelo menos __repr__ para fins de depuração.
+    *** Se os dois métodos estiverem declarados, __str__ terá preferência.
+
+    __eq__ compara o valor dos atributos dos objetos definidos na declaração do método
+    __hash__ Retorna um número inteiro(positivo ou negativo) único para o atributo do objeto
+    __bool__
+    __del__
+'''
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __repr__(self):
+        return f'__rpr__Person("{self.first_name}","{self.last_name}",{self.age})'
+    def __str__(self):
+        return f'__str__Person{self.first_name},{self.last_name},{self.age}'
+    def __eq__(self, other):
+        if isinstance(other, Person):
+            return self.age == other.age
+        return False
+    def __hash__(self):
+        return hash(self.first_name)
+
+    def __bool__(self):
+        if self.age < 18 or self.age > 65:
+            return False
+        return True
+''' def __eq__(self, other):
+        return self.age == other.age'''
+
+person = Person('John', 'Doe', 25)
+john = Person('John', 'Doe', 25)
+jane = Person('Jane', 'Doe', 25)
+mary = Person('Mary', 'Doe', 27)
+
+print(person)#Sem o método __str__ mostra o endereço de memória, com o método exibe uma string formatada
+
+print(john == jane)# True, sem o método __eq__ seria False, pois são objetos diferentes
+print(john == mary)# False
+print(john == 25)# False, pois o inteiro 25 não é um objeto da classe Person
+
+#Hash
+print(hash(john))# 7900537227961911167
+print(hash(jane))# 4012414512227509544
+print(hash(mary))# -1061679634863054657
+
