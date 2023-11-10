a7477a4110cce634d4accc7636bad78e953d266a
thiago-rocco <deroccotr@gmail.com>
2023-11-10 06:55:32 -0300

    CRUD POO com Python: Criada a classe User e os métodos connect e create_table

--- 14-Banco-de-dados/07-my_crud_sqlite_OO.py
+++ 14-Banco-de-dados/07-my_crud_sqlite_OO.py
@@ -1,3 +1,27 @@
 '''
     Operações de CRUD com o banco de dados Sqlite orientado a objetos
 '''
+import sqlite3
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    
+    @staticmethod
+    def connect():
+        conn = sqlite3.connect('database.db')
+        return conn
+    
+    @staticmethod
+    def create_table():
+        conn = User.connect()
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                email TEXT,
+                )
+                    ''')
+        
